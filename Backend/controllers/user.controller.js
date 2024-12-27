26b096380b9f093e0a3bd3ecc6157472b830f072
himudit <gargmudit662@gmail.com>
2024-12-27 20:47:35 +0530

    adding login & logout

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -20,6 +20,45 @@ module.exports.registerUser = async (req, res, next) => {
         const token = await user.generateAuthToken();
         res.status(201).json({ token, user });
     } catch (err) {
+        res.status(404).send({ error: err.message });
+        console.error(err);
+    }
+}
+
+module.exports.loginUser = async (req, res, next) => {
+    try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+        const { email, password } = req.body;
+        const user = await userModel.findOne({ email }).select('+password');
+
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        const isMatch = await user.comparePassword(password, user.password);
+
+        if (!isMatch) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+
+        const token = await user.generateAuthToken();
+
+        res.status(200).json({ token, user });
+
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+
+module.exports.getUserProfile = async (req, res, next) => {
+    try {
+        const user = req.user;
+        res.status(200).json(user);
+    } catch (err) {
         console.log(err);
     }
 }
--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { body } = require('express-validator');
 const userController = require('../controllers/user.controller')
+const authUser = require('../middlewares/auth.middleware')
 
 router.post('/register', [
     body('email').isEmail().withMessage('Invalid Email'),
@@ -9,6 +10,17 @@ router.post('/register', [
     body('password').isLength({ min: 8 }).withMessage('Password must be at 6 chars long'),
 ],
     userController.registerUser
+)
+
+router.post('/login', [
+    body('email').isEmail().withMessage('Invalid Email'),
+    body('password').isLength({ min: 8 }).withMessage('Password must be of 8 lenght')
+],
+    userController.loginUser
+)
+
+router.post('/profile', authMiddleware.authUser,
+    userController.getUserProfile
 )
 
 module.exports = router;
